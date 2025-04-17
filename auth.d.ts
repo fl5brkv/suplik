e4e564fa128784c73bae4bb0dff464c429532b09
fl5brkv <fl5brkv@proton.me>
2025-04-17 08:31:59 +0200

    add nuxthub, drizzle

---
+++ auth.d.ts
@@ -0,0 +1,18 @@
+// auth.d.ts
+declare module '#auth-utils' {
+  interface User {
+    userId: number;
+    email: string;
+    role: string;
+  }
+
+  interface UserSession {
+    // Add any fields relevant to the session here, if needed
+  }
+
+  interface SecureSessionData {
+    // Add any fields for secure session data here, if needed
+  }
+}
+
+export {};
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: false },
+  devtools: {enabled: false},
+
   future: {
     compatibilityVersion: 4,
   },
-})
+
+  modules: ['@nuxthub/core'],
+
+  hub: {
+    database: true,
+  },
+
+  nitro: {
+    experimental: {
+      tasks: true, // for db seed to work
+    },
+  },
+});
